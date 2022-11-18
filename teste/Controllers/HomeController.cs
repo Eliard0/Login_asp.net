fc67cd1d53f889be83e3b5fd2109cb1c434d15db
Eliardo <57414074+Eliard0@users.noreply.github.com>
2022-11-18 16:35:06 -0300

    Adicionar arquivos de projeto.

---
+++ teste/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using teste.Models;
+
+namespace teste.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModelcs loginModelcs)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (loginModelcs.Login == "adm" && loginModelcs.Senha == "123")
+                    {
+                        return RedirectToAction("index", "Home");
+                    }
+                    TempData["MensagemError"] = $"tente Novamente";
+                };
+                return View("index");
+            }
+            catch (Exception)
+            {
+                TempData["MensagemErro"] = $"erro";
+            }
+        }
+    }
+}
---
+++ teste/Models/LoginModelcs.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace teste.Models
+{
+    public class LoginModelcs
+    {
+        [Required(ErrorMessage = "Digite o login")]
+        public string Login { get; set; }
+        
+        [Required(ErrorMessage = "Digite a senha")]
+        public string Senha { get; set; }
+    }
+}
